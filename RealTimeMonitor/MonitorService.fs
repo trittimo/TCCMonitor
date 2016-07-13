ae9b0a58a7d95207eaf6eb368fb5cbe9c5bf8d40
Michael Trittin <michaeltrittin@gmail.com>
2016-07-13 10:55:51 -0400

    Can watch several directories and logs to a file

--- RealTimeMonitor/MonitorService.fs
+++ RealTimeMonitor/MonitorService.fs
@@ -9,19 +9,30 @@ open System
 module Constants =
     let ServiceName = "Realtime_Server_Monitor_1.0"
     let DisplayName = "Realtime Server Monitor"
+    let LogPath = @"C:\realtime_server_monitor_log.txt"
+    let WatchDirectories = [|(@"C:\Users\michael.trittin\Desktop", false)|] // path, recursive?
 
+module Logger =
+    let log s =
+        File.AppendAllText(Constants.LogPath, s + Environment.NewLine)
 // The windows service
 type MonitorWindowsService() =
     inherit ServiceBase(ServiceName = Constants.ServiceName)
 
     override x.OnStart(args) =
-        let watcher = new FileSystemWatcher()
-        watcher.Path <- @"C:\Users\michael.trittin\Desktop\"
-        watcher.EnableRaisingEvents <- true
-        watcher.IncludeSubdirectories <- false
-        watcher.Renamed.Add(fun _ ->
-            File.WriteAllText(@"C:\Users\michael.trittin\Desktop\service_log.txt",
-                "File event ocurred"))        
+        Logger.log "Started Realtime Server Monitor"
+        for towatch in Constants.WatchDirectories do
+            let dir, isRec = towatch
+            Logger.log("Creating a new FileSystemWatcher for " + dir)
+            let watcher = new FileSystemWatcher()
+            watcher.Path <- dir
+            watcher.EnableRaisingEvents <- true
+            watcher.IncludeSubdirectories <- isRec
+            let onChange(e: FileSystemEventArgs) =
+                Logger.log(e.FullPath + " was " + e.ChangeType.ToString())
+            watcher.Renamed.Add(onChange) |> ignore
+            watcher.Changed.Add(onChange) |> ignore
+            watcher.Created.Add(onChange) |> ignore
     override x.OnStop() = ()
 
 // Installer for the service
