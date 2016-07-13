90ee86f48ea25bc5561c7f17821369b3deeb0d78
Michael Trittin <michaeltrittin@gmail.com>
2016-07-13 10:11:09 -0400

    Apparently this is all that is necessary to run a service

--- RealTimeMonitor/MonitorService.fs
+++ RealTimeMonitor/MonitorService.fs
@@ -3,36 +3,20 @@ open System.ComponentModel
 open System.Configuration.Install
 open System.ServiceProcess
 open System.Collections.Generic
+open System.IO
 open System
-
-module MonitorBase =
-    type Monitor() =
-        member x.Start() = // TODO
-            printfn "Starting monitor..."
-            true
 
 module Constants =
     let ServiceName = "Realtime_Server_Monitor_1.0"
     let DisplayName = "Realtime Server Monitor"
-
-// The IMonitorService interface supplies the definitions for how we will interact with our service
-// The meat of the work will happen in the MonitorBase.Monitor class
-type IMonitorService =
-    abstract Start : unit -> bool
-
-// The service is where we start and stop our monitoring system, and also provide the data to any clients
-type MonitorService() =
-    inherit MarshalByRefObject()
-    let agent = new MonitorBase.Monitor()
-    interface IMonitorService with
-        member x.Start() = agent.Start()
 
 // The windows service
 type MonitorWindowsService() =
     inherit ServiceBase(ServiceName = Constants.ServiceName)
 
     override x.OnStart(args) =
-        printfn "Starting the service..."
+        File.WriteAllText(@"C:\Users\michael.trittin\Desktop\service_log.txt",
+                "Started monitor service")
     override x.OnStop() = ()
 
 // Installer for the service
