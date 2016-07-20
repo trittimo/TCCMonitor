247ea1aa22f7ebbb830315aaff9efdfe45e876ab
Michael Trittin <michaeltrittin@gmail.com>
2016-07-20 13:40:41 -0400

    Moving config.init out of config and into the monitorservice

--- RealTimeMonitor/Configuration.fs
+++ RealTimeMonitor/Configuration.fs
@@ -4,23 +4,8 @@ module Configuration =
     open System.IO
     type Config = {definitions:string[]; refresh:float}
     
-    let private load (path:string) =
+    let load (path:string) =
         if not (File.Exists(path)) then
             Logger.log ("Encountered exception loading configuration path: " + path + ". Could not find file. Stopping service...")
             failwith "Invalid configuration path"
         JsonConvert.DeserializeObject<Config>(File.ReadAllText(path))
-
-    let init (path:string) =
-        let config = load path
-        let loaders =
-            config.definitions |> Array.map(fun definition ->
-                AssemblyLoader.load definition)
-        let timer = new System.Timers.Timer()
-        timer.Elapsed.Add(fun _ ->
-            loaders |> Seq.iter(fun x ->
-                let exposed = (sprintf "%A" (x.Exposed))
-                Logger.log("Exposed: " + exposed)))
-        timer.Interval <- config.refresh
-        timer.Enabled <- true
-        timer.Start()
-        config
--- RealTimeMonitor/MonitorService.fs
+++ RealTimeMonitor/MonitorService.fs
@@ -8,12 +8,27 @@ module MonitorService =
     type Service() =
         inherit ServiceBase(ServiceName = Constants.ServiceName)
 
-        override x.OnStart(args) =
+        member this.Initialize (path:string) =
+            let config = Configuration.load path
+            let loaders =
+                config.definitions |> Array.map(fun definition ->
+                    AssemblyLoader.load definition)
+            let timer = new System.Timers.Timer()
+            timer.Elapsed.Add(fun _ ->
+                loaders |> Seq.iter(fun x ->
+                    let exposed = (sprintf "%A" (x.Exposed))
+                    Logger.log("Exposed: " + exposed)))
+            timer.Interval <- config.refresh
+            timer.Enabled <- true
+            timer.Start()
+            config
+
+        override this.OnStart(args) =
             Logger.log "Started Realtime Server Monitor"
-            let config = Configuration.init Constants.ConfigPath
+            let config = this.Initialize Constants.ConfigPath
             Logger.log ("Loaded configuration. Monitoring " + config.definitions.Length.ToString() + " assemblies: ")
             config.definitions |> Array.iter(fun x ->
                 Logger.log ("\tMonitoring " + x.ToString()))
 
-        override x.OnStop() =
+        override this.OnStop() =
             Logger.log "Stopping Realtime Server Monitor"
