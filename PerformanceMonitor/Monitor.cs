056504b8c9bf6464cb933d2c7b4f8d4ec03d8154
Michael Trittin <michaeltrittin@gmail.com>
2016-07-20 14:43:08 -0400

    Can monitor RAM and CPU usage

---
+++ PerformanceMonitor/Monitor.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace PerformanceMonitor
+{
+    public class Monitor
+    {
+        public static PerformanceCounter CpuCounter;
+        public static PerformanceCounter RamCounter;
+        static Monitor()
+        {
+            CpuCounter = new PerformanceCounter();
+
+            CpuCounter.CategoryName = "Processor";
+            CpuCounter.CounterName = "% Processor Time";
+            CpuCounter.InstanceName = "_Total";
+
+            RamCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        public static float GetCpuUsage()
+        {
+            return CpuCounter.NextValue();
+        }
+
+        public static float GetAvailableRam()
+        {
+            return RamCounter.NextValue();
+        }
+    }
+}
