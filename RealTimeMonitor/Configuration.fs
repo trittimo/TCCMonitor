1c4c06903f4058df4c45195b3ce776785ce2a9df
Michael Trittin <michaeltrittin@gmail.com>
2016-07-25 11:01:28 -0400

    Re-added Configuration because it didn't belong in assemblymonitor

---
+++ RealTimeMonitor/Configuration.fs
@@ -0,0 +1,10 @@
+﻿namespace AssemblyMonitor
+module Configuration =
+    open Newtonsoft.Json
+    open System.IO
+    type Config = {definitions:string[]; refresh:float}
+    
+    let load (path:string) =
+        if not (File.Exists(path)) then
+            failwith "Invalid configuration path"
+        JsonConvert.DeserializeObject<Config>(File.ReadAllText(path))
